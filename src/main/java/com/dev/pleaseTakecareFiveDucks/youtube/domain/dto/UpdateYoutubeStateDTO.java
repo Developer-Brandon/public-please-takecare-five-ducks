0dfada64d1d2b6571fd021605395d8306fa58a84
BrandonLee <brandon5959somi@gmail.com>
2022-11-23 18:44:22 +0900

    - Moving java file...

--- src/main/java/com/dev/pleaseTakecareFiveDucks/youtube/domain/dto/UpdateYoutubeStateDTO.java
+++ src/main/java/com/dev/pleaseTakecareFiveDucks/youtube/domain/dto/UpdateYoutubeStateDTO.java
@@ -1,6 +1,6 @@
 package com.dev.pleaseTakecareFiveDucks.youtube.domain.dto;
 
-import com.dev.webApp.util.YoutubeUseYnEnum;
+import com.dev.pleaseTakecareFiveDucks.youtube.util.YoutubeUseYnEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
