152892c682b9f445186ca1978e0035d0fe361d76
brandonLee <brandon5959somi@gmail.com>
2023-01-03 12:51:17 +0900

    * pleaseTakecareFiveDucks - flyway쪽 쿼리 프로시저로 예외처리

--- src/main/resources/db/migration/V1_1_2__insert_data_to_project_table.sql
+++ src/main/resources/db/migration/V1_1_2__insert_data_to_project_table.sql
@@ -1,14 +1,50 @@
 # user_type 삽입
-insert web_app_test2.tb_user_type(type)
-values('GUEST')
-,('ADMIN')
-,('SUPER_ADMIN');
+DELIMITER $$
 
-# user_type 삽입
-insert web_app_test2.tb_book_type(tag_korean_name, tag_english_name)
-values('스크립트', 'SCRIPT')
-, ('책요약본', 'BOOK_SUMMARY')
-,('책PDF', 'BOOK_PDF')
-,('공부요약본', 'STUDY_SUMMARY')
-,('강의요약본', 'LECTURE_SUMMARY')
-,('소설', 'NOVEL');
+DROP PROCEDURE IF EXISTS validate_data $$
+CREATE PROCEDURE validate_data()
+BEGIN
+
+    IF NOT EXISTS((SELECT *
+                   FROM web_app_test2.tb_user_type
+                   WHERE type = 'GUEST')) THEN
+
+        insert web_app_test2.tb_user_type(type)
+        values('GUEST')
+             ,('ADMIN')
+             ,('SUPER_ADMIN');
+
+    END IF;
+
+END $$
+
+CALL validate_data() $$
+
+DELIMITER ;
+
+# book_type 삽입
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS validate_data $$
+CREATE PROCEDURE validate_data()
+BEGIN
+
+    IF NOT EXISTS((SELECT *
+                   FROM web_app_test2.tb_book_type
+                   WHERE tag_korean_name = '스크립트')) THEN
+
+        insert web_app_test2.tb_book_type(tag_korean_name, tag_english_name)
+        values('스크립트', 'SCRIPT')
+             , ('책요약본', 'BOOK_SUMMARY')
+             ,('책PDF', 'BOOK_PDF')
+             ,('공부요약본', 'STUDY_SUMMARY')
+             ,('강의요약본', 'LECTURE_SUMMARY')
+             ,('소설', 'NOVEL');
+
+    END IF;
+
+END $$
+
+CALL validate_data() $$
+
+DELIMITER ;
